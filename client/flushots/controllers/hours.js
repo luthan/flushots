024750ac1bbe1db522dbd9203bcb441d3f4f3c2f
Paul Benetis <pauldke@yahoo.com>
2015-07-21 23:42:04 -0500

    fuck this

--- client/flushots/controllers/hours.js
+++ client/flushots/controllers/hours.js
@@ -1,15 +1,26 @@
 /// <reference path="../../../typings/tsd.d.ts"/>
 angular.module('flushotsApp').controller('HoursController', ['$scope', '$meteor', 'hourDisplayFilter', function($scope, $meteor, hourDisplayFilter){
 	var initializing = true;
-	$scope.hours = $meteor.collection(Hours).subscribe("hours");
+	//$scope.takenSlot = Meteor.subscribe("takenSlot","Paul");
+	$meteor.subscribe("hours").then(function(handle){
+		$scope.hours = $meteor.collection(Hours);
+		
+		// $scope.takenSlot = $scope.hours.find({slots:{$elemMatch:{"people":"Paul"}}});
+		// $scope.slot = Hours.findOne({slots:{$elemMatch:{"people":$scope.person}}});
+		$scope.slot = "hahaha";
+	});
 	
-	// console.log($scope.hours);
-	$scope.testhours = $meteor.collection(Hours);
+
+	// $meteor.subscribe("takenSlot","Paul").then(function(handle){
+	// 	handle.ready(function(){
+	// 		// console.log(handle);
+	// 	});
+	// });
 	
 	$scope.remove = function(hour){
 		$scope.hours.remove(hour);
 	}
-	// $scope.person = "";
+	$scope.person = "Paul";
 	$scope.removeAll = function(){
 		$scope.hours.remove();
 	}
@@ -18,26 +29,28 @@ angular.module('flushotsApp').controller('HoursController', ['$scope', '$meteor'
 		$scope.hours.push(newHour);
 		// console.log(newHour);
 	}
-	$scope.$watch('person',function(){
-		if(initializing){
-			initializing = false;
-		} else{
-			// console.log("changed");
-		}
-	});
+	// $scope.$watch('person',function(){
+	// 	if(initializing){
+	// 		initializing = false;
+	// 	} else{
+	// 		console.log("changed");
+	// 	}
+	// });
 	
 	$scope.loadPerson = function(person){
 		
 	}
 	
 	$scope.assignSlot = function(person, hour, slot){
-
-		Meteor.call("removePerson", person, function(error, data){
-			
+		Meteor.call("insertPerson", person, hour, slot, function(error, data){
+			if(error){
+				console.log(error);
+			} 
+			else{
+				console.log(data);
+				// $scope.slot = Hours.findOne({slots:{$elemMatch:{"people":data}}});
+				$scope.slot = "hahahah";
+			}
 		});
-		// Meteor.call("insertPerson", person, hour, slot, function(error, data){
-		// 	console.log(error);
-		// 	console.log(data);
-		// });
 	}
 }]);
--- model/hours.js
+++ model/hours.js
@@ -1,21 +1,12 @@
 Hours = new Mongo.Collection("hours");
+Slot = Hours;
+// Slot = new Mongo.Collection("hours");
 
 Meteor.methods({
-	insertPerson: function(person,hour,slot){
-		Hours.update( {"_id":hour._id, "slots.name": slot.name},
-			{$push : {"slots.$.people" : person}});
-		// var temp = Hours.findOne({"_id":hour._id});
-		// temp.slots[0].people.push(person);
-		// temp.update();
-		// console.log(temp);
-		return "hello";
-	},
-	removePerson: function(person){
-		Hours.update({}, { slots: { $pull: { people:"Paul" } } }, { multi: true });
-		// Hours.find({ slots : { $elemMatch : { people:"Paul" } } }) ;
-		// x.forEach(function(data){
-		// 	data.update({}, { $pull: { slots: { people:"Paul" } } }, { multi: true });
-		// 	console.log(data);
-		// });
+	insertPerson: function(person, hour, slot){
+		Hours.update({slots:{$elemMatch:{"people":person}}},{$pull:{"slots.$.people":person}}, function(){
+			Hours.update({"name":hour,"slots.name":slot},{$push:{"slots.$.people":person}});
+		});
+		return person;
 	}
 });
